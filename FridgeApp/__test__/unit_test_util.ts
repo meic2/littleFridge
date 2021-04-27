1cf2bc6016e292c93b4419e57b981eead8a605a6
mei chen <meic2@illinois.edu>
2021-04-27 00:16:08 -0500

    implement test

--- FridgeApp/__test__/unit_test_util.ts
+++ FridgeApp/__test__/unit_test_util.ts
@@ -1,6 +1,6 @@
-import {SpoonGrocery, SpoonFailure,} from '../types'
+import {SpoonGrocery, SpoonFailure, Recipe,} from '../types'
 import {
-  isSpoonGrocery, isSpoonFailure
+  isSpoonGrocery, isSpoonFailure, isRecipe, dateFormate
 } from '../utils';
 
 const failureMessage:SpoonFailure = {
@@ -12,6 +12,13 @@ const groceryInstance:SpoonGrocery ={
   title: "random Apple grocery blabla",
   spoon_id: 12345,
 };
+
+const recipeInstance:Recipe ={
+  createDate: "2012-01-02",
+  title: "random recipe",
+  spoon_id:-1
+};
+
 test('isSpoonGrocery_undefined', () => {
   const inst = undefined;
   expect(isSpoonGrocery(inst)).toStrictEqual(false);
@@ -47,4 +54,40 @@ test('isSpoonFailure_undefined', () => {
 
 test('isSpoonFailure_SpoonGrocery', () => {
   expect(isSpoonFailure(groceryInstance)).toStrictEqual(false);
+});
+
+/*
+new test for week 3
+ */
+test('isRecipe_true', () => {
+  expect(isRecipe(recipeInstance)).toStrictEqual(true);
+});
+
+test('isRecipe_string', () => {
+  const inst:string ="{'title':'new recipe'}";
+  expect(isRecipe(inst)).toStrictEqual(false);
+});
+
+test('isRecipe_undefined', () => {
+  expect(isRecipe(undefined)).toStrictEqual(false);
+});
+
+test('expire_dateFormat_correct', ()=>{
+  expect(dateFormate('2021-01-02')).toStrictEqual(true)
+});
+
+test('dateFormat_undefined', ()=>{
+  expect(dateFormate('')).toStrictEqual(false)
+});
+
+test('dateFormat_wrongFormat', ()=>{
+  expect(dateFormate('201-01-02')).toStrictEqual(false)
+});
+
+test('expire_dateFormat_wrongDate', ()=>{
+  expect(dateFormate('2019-01-02')).toStrictEqual(false)
+});
+
+test('create_dateFormat_wrongDate', ()=>{
+  expect(dateFormate('2019-01-02')).toStrictEqual(true)
 });
